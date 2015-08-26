cc49d451e5596ecda49a43f7f96e049a99b203f9
giner7415 <giner7415@gmail.com>
2015-08-25 23:19:34 -0500

    Actulizar el estilo del DOM

---
+++ script.js
@@ -0,0 +1,21 @@
+function primero(element){
+	element.textContent="El";
+	element.style.backgroundColor = "black";
+	element.style.color = "white";
+}
+
+function segundo(element){
+	element.textContent="poder";
+	element.style.backgroundColor = "white";	
+}
+
+function tercero(element){
+	element.textContent="de";
+	element.style.backgroundColor = "white";
+}
+
+function cuarto(element){
+	element.textContent="JavaScript";
+	element.style.backgroundColor = "yellow";
+	element.style.color = "black";
+}
